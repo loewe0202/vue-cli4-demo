257442ca58d0ebbfa7bb34a3f4fa365997ef0310
loewe0202 <loewe0202@163.com>
2019-12-21 17:38:42 +0800

    配置 externals 引入 cdn 资源

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
+/* jshint esversion: 6 */
+import Vue from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router';
+import store from './store';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount("#app");
+  render: h => h(App),
+}).$mount('#app');
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 /* jshint esversion: 6 */
+const path = require('path');
+const resolve = dir => path.join(__dirname, dir);
 const IS_PROD = ['production', 'prod'].includes(process.env.NODE_ENV);
 
 module.exports = {
@@ -10,16 +12,61 @@ module.exports = {
   productionSourceMap: !IS_PROD, // 生产环境的 source map
   parallel: require('os').cpus().length > 1,
   pwa: {},
+  configureWebpack: config => {
+    config.externals = {
+      vue: 'Vue',
+      'element-ui': 'ELEMENT',
+      'vue-router': 'VueRouter',
+      vuex: 'Vuex',
+      axios: 'axios',
+    };
+  },
+  chainWebpack: config => {
+    // 添加别名
+    config.resolve.alias
+      .set('vue$', 'vue/dist/vue.esm.js')
+      .set('@', resolve('src'))
+      .set('@assets', resolve('src/assets'))
+      .set('@scss', resolve('src/assets/scss'))
+      .set('@components', resolve('src/components'))
+      .set('@plugins', resolve('src/plugins'))
+      .set('@utils', resolve('src/utils'))
+      .set('@views', resolve('src/views'))
+      .set('@router', resolve('src/router'))
+      .set('@store', resolve('src/store'))
+      .set('@layouts', resolve('src/layouts'))
+      .set('@static', resolve('src/static'));
+
+    // 防止将某些 import 的包(package)打包到 bundle 中，而是在运行时(runtime)再去从外部获取这些扩展依赖
+    const cdn = {
+      // 访问https://unpkg.com/element-ui/lib/theme-chalk/index.css获取最新版本
+      css: ['//unpkg.com/element-ui@2.10.1/lib/theme-chalk/index.css'],
+      js: [
+        '//unpkg.com/vue@2.6.10/dist/vue.min.js', // 访问https://unpkg.com/vue/dist/vue.min.js获取最新版本
+        '//unpkg.com/vue-router@3.0.6/dist/vue-router.min.js',
+        '//unpkg.com/vuex@3.1.1/dist/vuex.min.js',
+        '//unpkg.com/axios@0.19.0/dist/axios.min.js',
+        '//unpkg.com/element-ui@2.10.1/lib/index.js',
+      ],
+    };
+
+    // 如果使用多页面打包，使用vue inspect --plugins查看html是否在结果数组中
+    config.plugin('html').tap(args => {
+      // html中添加cdn
+      args[0].cdn = cdn;
+      return args;
+    });
+  },
   devServer: {
-    // stats: { warningsFilter: (warning) => /Conflicting order between/gm.test(warning) },
-    open: process.platform === 'darwin',
+    open: true, // 是否打开浏览器
     disableHostCheck: true,
-    host: '0.0.0.0', // 允许外部ip访问
+    host: '0.0.0.0', // 允许外部 ip 访问
     port: 8022, // 端口
-    https: false, // 启用https
+    https: true, // 启用 https
+    hotOnly: true, // 热更新
     overlay: {
       warnings: true,
       errors: true,
-    }, // 错误、警告在页面弹出
+    }, // 让浏览器 overlay 同时显示警告和错误
   },
 };
